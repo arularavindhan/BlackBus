38d132253df64203941b0d9fc35b728723023c0f
arularavindhan <arularavindhan23@gmail.com>
2022-11-12 19:32:51 +0530

    3 methods added

---
+++ src/com/test/Git.java
@@ -0,0 +1,13 @@
+package com.test;
+
+public class Git {
+	private void a() {
+System.out.println("123");
+	}
+	private void b() {
+		System.out.println("12");
+			}
+	private void c() {
+		System.out.println("132");
+			}
+}
