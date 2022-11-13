73b9925e7c971f4e9e632975793c599e9c1b8407
arularavindhan <arularavindhan23@gmail.com>
2022-11-13 11:36:45 +0530

    3 methods added

--- src/com/test/Git.java
+++ src/com/test/Git.java
@@ -10,4 +10,14 @@ System.out.println("123");
 	private void c() {
 		System.out.println("132");
 			}
+	
+		private void d() {
+	System.out.println("123");
+		}
+		private void e() {
+			System.out.println("12");
+				}
+		private void f() {
+			System.out.println("132");
+}
 }
